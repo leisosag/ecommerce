660c37db03c8b3d568f8e69a60fb56a1ddaea5aa
Leila Sosa Gonzalez <leisosa93@gmail.com>
2021-07-06 23:35:49 -0300

    results from test video

--- ENTREGAFINAL/urls.py
+++ ENTREGAFINAL/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('JAGUARETE.urls')),
     path('JAGUARETE/', include('JAGUARETE.urls')),
     path('USUARIOS/', include('django.contrib.auth.urls')),
     path('USUARIOS/', include('USUARIOS.urls')),
